9ea60b07b23a9baeea24ccfd5e27250c16eb820f
hezhamen <hezhamen@gmail.com>
2022-07-13 03:14:26 +0300

    Initial Commit

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Cappuccino',
+      brand: 'Frothy Coffee',
+      flavors: ['milk', 'vanilla', 'caramel'],
+    },
+  ];
+
+  findAll(): Coffee[] {
+    return this.coffees;
+  }
+
+  findOne(id: number): Coffee {
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
+  }
+
+  update(id: number, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // update existing coffee
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
